6b0923a3350f2bfdecebf73ab7d4b5b51fcc29cb
matiaszzz <165030231+matiaszzz@users.noreply.github.com>
2025-11-08 12:16:00 -0300

    se conecto a la base de datos el backend AramayoMatias

---
+++ src/model/usuarios.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const routes = express.Router();
+
+//modelo de datos
+const mongoose = require('mongoose');
+const esquema = mongoose.Schema;
+
+const esquemaUsuario = new esquema({
+    username: String,
+    password: String,
+    rol: String,
+    name: String
+});
+
+const listaUsuarios = mongoose.model('users', esquemaUsuario);
+
+routes.get('/obtenerUsuario', async (req, res) => {
+    try{
+        const docs = await listaUsuarios.find();
+        res.send(docs);
+
+    }catch (error){
+        console.error("Error al obtener usuario", error);
+        res.status(500).send({ message: "Error interno del servidor al obtener usuarios", error: error})
+    }
+});
+
+module.exports = routes;
